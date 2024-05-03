d38147111ae178bed8178c2eb1091a8ebcbbf1ec
Leila Mirelle Da Silva Vasconcelos <l.silva.vasconcelos@avanade.com>
2024-05-02 21:42:47 -0300

    Terceira Atualização

--- ProjetoControleVendas/obj/Debug/net8.0/MiniMundo.AssemblyInfo.cs
+++ ProjetoControleVendas/obj/Debug/net8.0/MiniMundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630b1ea6f34191645f75683fff6699e30163eef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76ec397f58decb2b4e95fd668735a6df1415609d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
